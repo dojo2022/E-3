228e25677c53612a6543fe966ffff006d498d986
dojo2022-suzuki <103238749+dojo2022-suzuki@users.noreply.github.com>
2022-06-13 15:23:51 +0900

    固定費変動費

--- src/src/servlet/F_registServlet.java
+++ src/src/servlet/F_registServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.FixedDao;
+import model.Fixed;
+import model.Result;
 
 /**
  * Servlet implementation class F_registServlet
@@ -13,29 +19,43 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/F_registServlet")
 public class F_registServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public F_registServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 登録ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/v_regist.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+				request.setCharacterEncoding("UTF-8");
+
+				String f_date = request.getParameter("f_date");
+				String f_category = request.getParameter("f_category");
+				String f_memo = request.getParameter("f_memo");
+				int f_cost = Integer.parseInt(request.getParameter("f_cost"));
+
+				// 登録処理を行う
+				FixedDao fDao = new FixedDao();
+				if (fDao.insert(new Fixed(f_date, f_category, f_memo, f_cost))) { // 登録成功
+					request.setAttribute("result",
+							new Result("登録成功しました", "/selfManagement/F_registServlet", "/selfManagement/H_listServlet"));
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+					dispatcher.forward(request, response);
+				} else { // 登録失敗
+					request.setAttribute("result",
+							new Result("登録失敗しました", "/selfManagement/V_registServlet", "/selfManagement/H_listServlet"));
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+					dispatcher.forward(request, response);
+				}
 	}
 
 }
--- src/src/servlet/V_registServlet.java
+++ src/src/servlet/V_registServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.VariableDao;
+import model.Result;
 
 /**
  * Servlet implementation class V_registServlet
@@ -13,29 +18,43 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/V_registServlet")
 public class V_registServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public V_registServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 登録ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/v_regist.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+
+		String v_date = request.getParameter("v_date");
+		String v_category = request.getParameter("v_category");
+		String v_memo = request.getParameter("v_memo");
+		int v_cost = Integer.parseInt(request.getParameter("v_cost"));
+
+		// 登録処理を行う
+		VariableDao vDao = new VariableDao();
+		if (vDao.insert(new Variable(v_date, v_category, v_memo, v_cost))) { // 登録成功
+			request.setAttribute("result",
+					new Result("登録成功しました", "/selfManagement/V_registServlet", "/selfManagement/H_listServlet"));
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		} else { // 登録失敗
+			request.setAttribute("result",
+					new Result("登録失敗しました", "/selfManagement/V_registServlet", "/selfManagement/H_listServlet"));
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
