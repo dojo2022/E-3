29ac0a254533375b11f8755aba477a9da1fc3daa
dojo2022-naotoshimohinata <shimohinata-naoto-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-13 15:27:11 +0900

    JSP途中だよ

--- src/WebContent/WEB-INF/jsp/s_regist.jsp
+++ src/WebContent/WEB-INF/jsp/s_regist.jsp
@@ -4,9 +4,31 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title></title>
 </head>
 <body>
-
+<form method="POST" action="/selfManagement/S_registServlet">
+<table>
+<tr>
+<td>日付<input type="date"></input></td></tr>
+<tr>
+<td>カテゴリー<select name="s_category"size="1">
+<option value="予定1">遊び</option>
+<option value="予定2">休み</option>
+<option value="予定3">デート</option>
+<option value="予定4">飲み会</option>
+<option value="予定5">旅行</option>
+<option value="予定6">支払い期限</option>
+<option value="予定7">イベント</option>
+<option value="予定8">その他</option>
+</select></td>
+</tr>
+<tr>
+<td>メモ<input type="text" memo="memo"></td>
+</tr>
+</table>
+</form>
+<a href="/selfManagement/S_registServlet">登録</a>
+<input type="reset">
 </body>
 </html>
--- src/src/servlet/S_registServlet.java
+++ src/src/servlet/S_registServlet.java
@@ -1,11 +1,18 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.ScheduleDAO;
+import Schedule;
+import model.Result;
 
 /**
  * Servlet implementation class S_registServlet
@@ -13,29 +20,57 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/S_registServlet")
 public class S_registServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public S_registServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		/**
+		 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// もしもログインしていなかったらログインサーブレットにリダイレクトする
+			HttpSession session = request.getSession();
+			if (session.getAttribute("id") == null) {
+				response.sendRedirect("/selfManagement/LoginServlet");
+				return;
+			}
+
+			// 登録ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/selfManagement/jsp/s_regist.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// もしもログインしていなかったらログインサーブレットにリダイレクトする
+			HttpSession session = request.getSession();
+			if (session.getAttribute("id") == null) {
+				response.sendRedirect("/selfManagement/LoginServlet");
+				return;
+			}
+			request.setCharacterEncoding("UTF-8");
+			String date = request.getParameter("Date");
+			String s_category = request.getParameter("S_CATEGORY");
+			String memo = request.getParameter("MAMO");
+
+			// 登録処理を行う
+			DAO bDao = new BcDAO();
+			if (bDao.insert(new Bc(number, cname, name, address, tel))) {	// 登録成功
+				request.setAttribute("result",
+				new Result("登録成功！", "レコードを登録しました。", "/simpleBC/MenuServlet"));
+			}
+			else {												// 登録失敗
+				request.setAttribute("result",
+				new Result("登録失敗！", "レコードを登録できませんでした。", "/simpleBC/MenuServlet"));
+			}
+
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+
+		}
 	}
 
-}
