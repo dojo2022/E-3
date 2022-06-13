4778a329e8c1edccfa5b7de60a22c7c0f30a9b51
dojo2022-morimoto <103239362+dojo2022-morimoto@users.noreply.github.com>
2022-06-13 15:21:50 +0900

    s_list更新１

--- src/src/servlet/S_listServlet.java
+++ src/src/servlet/S_listServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+//import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+//import dao.BcDAO;
+//import model.Bc;
 
 /**
  * Servlet implementation class S_listServlet
@@ -13,21 +19,30 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/S_listServlet")
 public class S_listServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public S_listServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+/*		response.getWriter().append("Served at: ").append(request.getContextPath());
+		HttpSession session = request.getSession();
+		if (session.getAttribute("id") == null) {
+			response.sendRedirect("/selfManagement/LoginServlet");
+			return;
+		}
+*/
+
+		// スケジュール一覧ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/s_list.jsp");
+		dispatcher.forward(request, response);
+
+
+// 検索処理を行う
+//BcDAO bDao = new BcDAO();
+//List<Bc> cardList0 = bDao.display();
+// 検索結果をリクエストスコープに格納する
+//request.setAttribute("cardList0", cardList0);
+
+
 	}
 
 	/**
