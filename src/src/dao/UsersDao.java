beee3f6e03be5de897fa8f4e40a9277dad7b060a
dojo2022-morimoto <103239362+dojo2022-morimoto@users.noreply.github.com>
2022-06-15 10:59:23 +0900

    ユーザーだお

--- src/src/dao/UsersDao.java
+++ src/src/dao/UsersDao.java
@@ -67,4 +67,70 @@ public class UsersDao {
 			// 結果を返す
 			return userList;
 		}
+
+
+		// 引数cardで指定されたレコードを登録し、成功したらtrueを返す
+		public boolean insert(Users user) {
+			Connection conn = null;
+			boolean result = false;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/SM", "sa", "kawasaki");
+
+				// SQL文を準備する
+				String sql = "insert into user (login_id, password, constitution) values (?, ?, ?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				if (user.getLogin_id() != null && !user.getLogin_id().equals("")) {
+					pStmt.setString(1, user.getLogin_id());
+				}
+				else {
+					pStmt.setString(1, null);
+				}
+				if (user.getPassword() != null && !user.getPassword().equals("")) {
+					pStmt.setString(2, user.getPassword());
+				}
+				else {
+					pStmt.setString(2, null);
+				}
+				if (user.getConstitution() != null && !user.getConstitution().equals("")) {
+					pStmt.setString(3, user.getConstitution());
+				}
+				else {
+					pStmt.setString(3, null);
+				}
+
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					}
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+
 }
--- src/src/model/Users.java
+++ src/src/model/Users.java
@@ -2,15 +2,22 @@ package model;
 
 public class Users {
 	private String user_id;
+	private String login_id;
+	private String password;
 	private String reason;
 	private int goal;
 	private String deadline;
 	private int salary;
 	private String constitution;
 
-	public Users(String user_id, String reason, int goal, String deadline, int salary, String constitution) {
+
+
+	public Users(String user_id, String login_id, String password, String reason, int goal, String deadline, int salary,
+			String constitution) {
 		super();
 		this.user_id = user_id;
+		this.login_id = login_id;
+		this.password = password;
 		this.reason = reason;
 		this.goal = goal;
 		this.deadline = deadline;
@@ -18,15 +25,32 @@ public class Users {
 		this.constitution = constitution;
 	}
 
-
 	public Users() {
 		super();
 		this.user_id = "";
+		this.login_id = "";
+		this.password = "";
 		this.reason = "";
 		this.goal = 0;
 		this.deadline = "";
 		this.salary = 0;
 		this.constitution = "";
+	}
+
+	public String getLogin_id() {
+		return login_id;
+	}
+
+	public void setLogin_id(String login_id) {
+		this.login_id = login_id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getUser_id() {
