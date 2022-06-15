872c81d275f01cb938d9ac2465874143d098215c
dojo2022-ishikura <ishikura-reina-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-15 16:59:11 +0900

    例外が出るU_updateサーブレット

--- src/WebContent/WEB-INF/jsp/u_regist.jsp
+++ src/WebContent/WEB-INF/jsp/u_regist.jsp
@@ -10,20 +10,20 @@
 <h1>Self Managment</h1>
  <main>
   <h2>新規登録してください</h2>
-   <form method="GET" action="/selfManagement/U_registServlet">
+   <form method="POST" action="/selfManagement/U_registServlet">
    <p>ID:
-   <input type="text" name="name"></p>
+   <input type="text" name="login_id"></p>
    <p>PW:
-   <input type="text" name="name"></p>
+   <input type="password" name="password"></p>
    <label for="kind">暑がり・寒がり</label><br>
-   <select id="kind" name="Kind">
+   <select id="kind" name="constitution">
     <option value="heat">暑がり</option>
     <option value="cold">寒がり</option>
     <option value="neither">どちらでもない</option>
    </select>
    <p>全項目入力してください</p>
-   <p><input type="button" name="regist" value="登録">
-   <p><input type="button" name="login" value="ログインへ">
+   <p><input type="submit" name="regist" value="登録">
+   <p><input type="button" onclick="location.href='/selfManagement/LoginServlet'" value="ログインへ">
    </form>
  </main>
 </body>
--- src/src/servlet/U_registServlet.java
+++ src/src/servlet/U_registServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.UsersDao;
 import model.Result;
@@ -27,51 +26,39 @@ public class U_registServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		/*
-		if (session.getAttribute("login_id") == null) {
-			response.sendRedirect("/selfManagement/LoginServlet");
-			return;
-		}
-		*/
+
 		// 登録ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/u_regist.jsp");
 		dispatcher.forward(request, response);
-
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	throws ServletException, IOException {
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
 		String login_id = request.getParameter("login_id");
 		String password = request.getParameter("password");
 		String constitution = request.getParameter("constitution");
-		//↑？？？
 
-		// 登録処理を行う(改造)
+		// 登録処理を行う
 		UsersDao uDao = new UsersDao();
-		if (request.getParameter("regist").equals("登録")) {
-			if (uDao.insert(new Users(login_id, password, constitution))) {	// 登録成功
-				request.setAttribute("result",
-				new Result("登録しました",login_id + "を登録しました。", "/selfManagement/LoginServlet"));
-			}
-			else {												// 登録失敗
-				request.setAttribute("result",
-				new Result("登録失敗！", "会員登録できませんでした。", "/selfManagement/LoginServlet"));
-			}
-		} else {
-			selfManagement/LoginServlet
-
-		}
+		if (uDao.insert(new Users(login_id, password, constitution))) {	// 登録成功
+			request.setAttribute("result",
+			new Result("登録しました",login_id + "を登録しました。", "/selfManagement/LoginServlet"));
 			// 結果ページにフォワードする
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
 			dispatcher.forward(request, response);
 		}
+		else {// 登録失敗
+			request.setAttribute("result",
+			new Result("登録失敗！", "会員登録できませんでした。", "/selfManagement/LoginServlet"));
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		}
+
 	}
 }
