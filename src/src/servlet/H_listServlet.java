1ca43f733303ed9d90240ebb5867481c9c90b6dc
dojo2022-ishikura <ishikura-reina-plusdojo2022@seplus2016.onmicrosoft.com>
2022-06-22 15:54:45 +0900

    家計簿一覧目標サマリー表示

--- src/src/servlet/H_listServlet.java
+++ src/src/servlet/H_listServlet.java
@@ -14,8 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.FixedDao;
+import dao.UserDao;
 import dao.VariableDao;
 import model.Fixed;
+import model.User;
 import model.Variable;
 
 /**
@@ -50,12 +52,31 @@ public class H_listServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		int user_id = (int)sessionuser.getAttribute("user_id");
 
-		//デフォルトのカレンダークラスを宣言、下のDateはセット
-		SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM");
-		//Calendar calendar = Calendar.getInstance();
+		//今日の日付を取得
 		Date date = new Date();
+
+		//値の書式を指定
+		SimpleDateFormat df = new SimpleDateFormat("MM");
+		SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM");
+		SimpleDateFormat df3 = new SimpleDateFormat("yyyy");
+
+		//検索処理を行う
+		UserDao uDao = new UserDao();
+		User user = uDao.display(user_id);
+
+		//日付取得
+		Date date1 = uDao.deadline(user_id);
+		int d1 = Integer.parseInt(df.format(date1));	//達成期限（月）
+		int d2 = Integer.parseInt(df.format(date));	//今月
+		int y1 = Integer.parseInt(df3.format(date1));	//達成期限（年）
+		int y2 = Integer.parseInt(df3.format(date));	//今年
+
+		int deadline = d1 - d2;
+		int year = y1 - y2;
+
 		String d3 = df2.format(date);
 
+		//残高を計算する
 		//変動費検索処理を行う
 		VariableDao vDao = new VariableDao();
 		List<Variable> variableList = vDao.variable(new Variable(0, d3, "", "", 0, user_id));
@@ -67,6 +88,12 @@ public class H_listServlet extends HttpServlet {
 		List<Fixed> fixedList = fDao.fixed(new Fixed(0, "", "", "", 0, user_id));
 		//検索結果をリクエストスコープに格納する
 		request.setAttribute("fixedList", fixedList);
+
+		//検索結果をリクエストスコープに格納する
+		request.setAttribute("deadline", deadline);	//達成期限と今月の差
+		request.setAttribute("year", year);			//達成期限（年）
+		request.setAttribute("date1", date1);       //達成期限(月)
+		request.setAttribute("user", user);			//ユーザーデータ
 
 		// メニューページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/h_list.jsp");
